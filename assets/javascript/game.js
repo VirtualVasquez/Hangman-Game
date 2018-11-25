f802f03b76792aba26abe218c4d0a5a8f7060b72
VirtualVasquez <mxv647@med.miami.edu>
2018-11-25 15:32:39 -0500

    refresh of skills, optimizing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,14 @@
 //---------------------------------------
 // Used to record how many times a letter can be pressed
 var doubleWord = ['a','b','c',
-				  'd','e','f',
-				  'g','h','i',
-				  'j','k','l',
-				  'm','n','o',
-				  'p','q','r',
-				  's','t','u',
-				  'v','w','x',
-				  'y','z'];
+          'd','e','f',
+          'g','h','i',
+          'j','k','l',
+          'm','n','o',
+          'p','q','r',
+          's','t','u',
+          'v','w','x',
+          'y','z'];
 //Holds the all the names that can be generated
 var nameBank =['flowey', 'toriel', 'sans', 'papyrus', 'undyne'];
 //Holds chosenName
@@ -31,135 +31,166 @@ var rightGuessCounter = 0;
 //This secdtion is used to DEFINE the functions that need to be used in the game.
 //As is, these functions will not self-initiate.
 //----------------------------------------
+
+// DOM Manipulators
+function updateWord(){
+  document.getElementById('wordToGuess').innerHTML = ("Your Word to Guess: " + "<br>" + blanksAndSuccesses.join(' '))
+}
+
+function updateWins(){
+  document.getElementById('winCounter').innerHTML = ("Wins: " + winCount)
+}
+function updateLosses(){
+  document.getElementById('loseCounter').innerHTML = ("Losses: " + loseCount)
+}
+
+function updateNumGuesses(){
+  document.getElementById('numGuesses').innerHTML = ("Number of Guesses Left: " + "<br>" + guessesLeft)
+}
+
+function updateWrongGuesses(){
+  document.getElementById('wrongGuesses').innerHTML = ("Wrong Letters Guessed: " + "<br>" + wrongLetters)
+}
+
+function noGuesses(){
+  document.getElementById('wrongGuesses').innerHTML = ("Wrong Letters Guessed: " + "<br>" + "You Haven't Tried Yet")
+}
+
+
+
 //the reset function will begin upon the page loading, or a new game starts, win or loss
 function reset()
 {
-	//Chooses word randomly from the nameBank
-	chosenName = nameBank[Math.floor(Math.random() * nameBank.length)];
-	//Splits the choosen word into individual letters
-	lettersInWord = chosenName.split('');
-	//number of letters of the chosenName equals the number of underscores generated
-	numBlanks = lettersInWord.length;
-	
-	//RESET
-	//these variables are what the counters and guessing space start at upon loading
-	//OR beginning a new game, due to win or loss
-	//var wincount is ommitted, as to be able to retain the number of wins between games
-	//===========================================================
-	letterGuessed = 0;
-	rightGuessCounter = 0;
-	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
-	test=false;
-	//"StartGame();" is the next function that loads upon "reset()" finishing
-	//if it weren't here, then the page would remain static.
-	startGame();
+  //Chooses word randomly from the nameBank
+  chosenName = nameBank[Math.floor(Math.random() * nameBank.length)];
+  //Splits the choosen word into individual letters
+  lettersInWord = chosenName.split('');
+  //number of letters of the chosenName equals the number of underscores generated
+  numBlanks = lettersInWord.length;
+  
+  //RESET
+  //these variables are what the counters and guessing space start at upon loading
+  //OR beginning a new game, due to win or loss
+  //var wincount is ommitted, as to be able to retain the number of wins between games
+  //===========================================================
+  letterGuessed = 0;
+  rightGuessCounter = 0;
+  guessesLeft = 9;
+  wrongLetters =[];
+  blanksAndSuccesses =[];
+  doubleWord = ['a','b','c',
+            'd','e','f',
+            'g','h','i',
+            'j','k','l',
+            'm','n','o',
+            'p','q','r',
+            's','t','u',
+            'v','w','x',
+            'y','z'];
+  test=false;
+  //"StartGame();" is the next function that loads upon "reset()" finishing
+  //if it weren't here, then the page would remain static.
+  startGame();
 }
+
+
 function startGame()
 {
-	//Chooses word randomly from the nameBank
-	chosenName = nameBank[Math.floor(Math.random() * nameBank.length)];
-	//Splits the chosenName into individual letters
-	lettersInWord = chosenName.split('');
-	//Get the number of blanks
-	numBlanks = lettersInWord.length;
-	
-	//RESET
-	//===========================================================
-	letterGuessed = 0;
-	rightGuessCounter = 0;
-	guessesLeft = 9;
-	wrongLetters =[];
-	blanksAndSuccesses =[];
-	doubleWord = ['a','b','c',
-					  'd','e','f',
-					  'g','h','i',
-					  'j','k','l',
-					  'm','n','o',
-					  'p','q','r',
-					  's','t','u',
-					  'v','w','x',
-					  'y','z'];
+  //Chooses word randomly from the nameBank
+  chosenName = nameBank[Math.floor(Math.random() * nameBank.length)];
+  //Splits the chosenName into individual letters
+  lettersInWord = chosenName.split('');
+  //Get the number of blanks
+  numBlanks = lettersInWord.length;
+  
+  //RESET
+  //===========================================================
+  letterGuessed = 0;
+  rightGuessCounter = 0;
+  guessesLeft = 9;
+  wrongLetters =[];
+  blanksAndSuccesses =[];
+  doubleWord = ['a','b','c',
+            'd','e','f',
+            'g','h','i',
+            'j','k','l',
+            'm','n','o',
+            'p','q','r',
+            's','t','u',
+            'v','w','x',
+            'y','z'];
 
-	//Populate blanks
-	for(var i = 0; i< numBlanks; i++)
-	{
-		blanksAndSuccesses.push('_');
-		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
-	}
+  //Populate blanks
+  for(var i = 0; i< numBlanks; i++)
+  {
+    blanksAndSuccesses.push('_');
+    document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
+  }
 
-	//Changes HTML 
-	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-	document.getElementById('numGuesses').innerHTML = guessesLeft;
-	document.getElementById('winCounter').innerHTML = winCount;
-	document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+  //Changes HTML 
+  updateWord();
+  updateWins();
+  updateLosses();
+  updateNumGuesses();
+  noGuesses()
 }
 
 function compareLetters(userKey)
 {
-				//Wanted to define var small = userKey.toLowerCase();
-				//however, replacing instances of "userKey" with "small"
-				//did not yield success
+        //Wanted to define var small = userKey.toLowerCase();
+        //however, replacing instances of "userKey" with "small"
+        //did not yield success
 
-				//If user key exist in choosen word then perform this function 
-				if(chosenName.indexOf(userKey) > -1)
-				{
-					//Loops depending on the amount of blanks 
-					for(var i = 0; i < numBlanks; i++)
-					{
-						//Fills in right index with user key
-						if(lettersInWord[i] === userKey)
-						{
-							rightGuessCounter++;
-							blanksAndSuccesses[i] = userKey;
-							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-						}	
-					}
-				}
-				//Wrong Keys
-				else
-				{
-					wrongLetters.push(userKey);
-					guessesLeft--;
-					//Changes HTML
-					document.getElementById('numGuesses').innerHTML = guessesLeft;
-					document.getElementById('wrongGuesses').innerHTML = wrongLetters;
-				}
-			
-			
-		
+        //If user key exist in choosen word then perform this function 
+        if(chosenName.indexOf(userKey) > -1)
+        {
+          //Loops depending on the amount of blanks 
+          for(var i = 0; i < numBlanks; i++)
+          {
+            //Fills in right index with user key
+            if(lettersInWord[i] === userKey)
+            {
+              rightGuessCounter++;
+              blanksAndSuccesses[i] = userKey;
+              updateWord();
+            } 
+          }
+        }
+        //Wrong Keys
+        else
+        {
+          wrongLetters.push(userKey);
+          guessesLeft--;
+          //Changes HTML
+          updateNumGuesses();
+          updateWrongGuesses();
+        }
+      
+      
+
 }
 function winLose()
 {
-	// When number blanks if filled with right words then you win
-	if(rightGuessCounter === numBlanks)
-	{
-		//Counts Wins 
-		winCount++;
-		//Changes HTML
-		document.getElementById('winCounter').innerHTML = winCount;
-		reset();
-	}
-	// When number of Guesses reaches 0 then You lose
-	else if(guessesLeft === 0)
-	{
-
-		reset();
-	}
+  // When number blanks if filled with right words then you win
+  if(rightGuessCounter === numBlanks)
+  {
+    //Counts Wins 
+    winCount++;
+    //Changes HTML
+    reset();
+  }
+  // When number of Guesses reaches 0 then You lose
+  else if(guessesLeft === 0)
+  {
+    //Counts Losses
+    loseCount++;
+    //Changes HTML
+    reset();
+  }
 }
 
 //MAIN PROCCESS
-//-------------------------------------------	
+//------------------------------------------- 
 //Initiates the Code; loops back up on the javascript
 //to where the function startGame was first defineed,
 //then moves down sequentially
@@ -167,18 +198,18 @@ startGame();
 
 document.onkeyup = function(event)
 {
-	test = true;
-	var letterGuessed = event.key; 
-	for(var i = 0; i < doubleWord.length; i++)
-	{	
-		if(letterGuessed === doubleWord[i] && test === true)
-		{
-			var spliceDword = doubleWord.splice(i,1);
-			compareLetters(letterGuessed);
-			winLose();
-		}
-	}		
-		
+  test = true;
+  var letterGuessed = event.key; 
+  for(var i = 0; i < doubleWord.length; i++)
+  { 
+    if(letterGuessed === doubleWord[i] && test === true)
+    {
+      var spliceDword = doubleWord.splice(i,1);
+      compareLetters(letterGuessed);
+      winLose();
+    }
+  }   
+    
 }
 
 //had trouble understanding event.key, userKey.
