8705b580ed6926f0fc761e88adbe5a8bae3eb6dd
VirtualVasquez <mxv647@med.miami.edu>
2020-10-12 17:12:31 -0400

    init commit for react overhaul of app

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,5 @@
+<div id="startScreen" class="container">
+<div id="startText" class="blink"><p>Press Any Key To Start</p></div>
+<audio class="fx" id="firstSound" src="assets/sounds/menu.mp3" autoplay loop>
+</audio>
+</div>
