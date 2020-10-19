6045acc89f1bc23935a222ba77e85b240e5ee512
VirtualVasquez <mxv647@med.miami.edu>
2020-10-19 19:56:58 -0400

    MQs and .cols work fine, minus text. Revisit later, move onto functional sections

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -14,85 +14,57 @@ const Game = props =>{
   <div>
     <Container>
       <Row id="master">
-        <Col xs={6} id="backdrop">
+        <Col xs={6} md={12} id="backdrop">
           <Image src={require('../../images/sans.png')} id="game-pic" fluid />
         </Col>
-        <Col xs={6} id="puzzle">
+        <Col xs={6} md={12} id="puzzle">
+          <p>_ _ _ _ _ _ _ _  _ _ _ _ </p>
         </Col>
         <Col xs={12} id="stats">
-          <Row>
-            <Col xs={4}>
-              <p id="wins">WINS LV 00</p>
-            </Col>
-            <Col xs={1}>
+            <p id="wins">WINS LV 00</p>
+            <div>
               <p>HP</p>
-            </Col>
-            <Col id="prog-col" xs={2}>
               <ProgressBar id="progress" now={50} variant="warning"/>
-            </Col>
-            <Col xs={2}>
               <p>6 / 6</p>
-            </Col>
-          </Row>
+            </div>
         </Col>
         <Col xs={12} id="buttons">
-        </Col>      
-      </Row>
-
-
-      {/* <Row id="backdrop">
-        <Col>
-            <Image src={require('../../images/sans.png')} id="game-pic" fluid />
-        </Col>
-      </Row>
-      <Row id="puzzle">
-        <Col>
-        <p>_ _ _ _ _ _ _ _ _ _ _ _ _ _ </p>
-        </Col>
-      </Row>
-      <Row id="stats">
-        <Col>
-          <p id="wins">WINS 00</p>
-        </Col>
-        <Col>
-            <Row>
-              <Col md={2}>
-                <p>HP</p>
-              </Col>
-              <Col md={4} id ="bar-col">
-                    <ProgressBar id="progress" now={50} variant="warning"/>
-              </Col>
-              <Col md={6}>
-                <p>6 / 6</p>
-              </Col>
-            </Row>
-        </Col>
-        <Col>
-        </Col>
-      </Row>
-      <Row id="buttons">
-        <Col lg={3}>
+          {/* <Row>
+            <Col xs={3}>
+              <Card>
+                <Card.Body>FIGHT</Card.Body>
+              </Card>
+            </Col>
+            <Col xs={3}>
+              <Card>
+                <Card.Body>FIGHT</Card.Body>
+              </Card>
+            </Col>
+            <Col xs={3}>
+              <Card>
+                <Card.Body>FIGHT</Card.Body>
+              </Card>
+            </Col>
+            <Col xs={3}>
+              <Card>
+                <Card.Body>FIGHT</Card.Body>
+              </Card>
+            </Col>
+          </Row> */}
           <Card>
             <Card.Body>FIGHT</Card.Body>
           </Card>
-        </Col>
-        <Col lg={3}>
           <Card>
             <Card.Body>ACT</Card.Body>
           </Card>
-        </Col>
-        <Col lg={3}>
           <Card>
             <Card.Body>ITEM</Card.Body>
           </Card>
-        </Col>
-        <Col lg={3}>
           <Card>
             <Card.Body>MUSIC</Card.Body>
           </Card>
-        </Col>
-      
-      </Row> */}
+        </Col>      
+      </Row>
     </Container>
   </div>
   )
