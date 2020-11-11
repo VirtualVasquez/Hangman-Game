e4ca66a66d75e70f6389bddeac2cccb83f4f0ba4
VirtualVasquez <mvasq95@hotmail.com>
2020-11-11 15:59:00 -0500

    Updates

---
+++ precache-manifest.f35014698cd5948fae758c3f1a56f78b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf8bb63e507cf11bd995947316dec4d",
+    "url": "/Hangman-Game/index.html"
+  },
+  {
+    "revision": "828360a47ab1cd51091a",
+    "url": "/Hangman-Game/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "62254a6462946301bec9",
+    "url": "/Hangman-Game/static/css/main.3d48a427.chunk.css"
+  },
+  {
+    "revision": "828360a47ab1cd51091a",
+    "url": "/Hangman-Game/static/js/2.6c0e2ca1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Hangman-Game/static/js/2.6c0e2ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62254a6462946301bec9",
+    "url": "/Hangman-Game/static/js/main.35ecec94.chunk.js"
+  },
+  {
+    "revision": "2b98a14368f93bc5f624",
+    "url": "/Hangman-Game/static/js/runtime-main.fd43e129.js"
+  },
+  {
+    "revision": "7890fc69986c31cf5a4dbceb492a9f97",
+    "url": "/Hangman-Game/static/media/DeterminationMonoWeb.7890fc69.ttf"
+  },
+  {
+    "revision": "f849f7f9f1078b7a11bf4b04671dfc89",
+    "url": "/Hangman-Game/static/media/dance.f849f7f9.gif"
+  },
+  {
+    "revision": "3b8803bcefeadaa028b03cfbbe994b18",
+    "url": "/Hangman-Game/static/media/flowey.3b8803bc.png"
+  },
+  {
+    "revision": "322d8d3c9cf098c6c0cf8af03d94b487",
+    "url": "/Hangman-Game/static/media/homescreen.322d8d3c.jpg"
+  },
+  {
+    "revision": "a318c669d0fda74a59219652a4f95f56",
+    "url": "/Hangman-Game/static/media/lost.a318c669.png"
+  },
+  {
+    "revision": "82e7844fb221cd5c9af01334cb231c2b",
+    "url": "/Hangman-Game/static/media/napstablook.82e7844f.png"
+  },
+  {
+    "revision": "99e32558a87b142923a4e75eae088b9c",
+    "url": "/Hangman-Game/static/media/undyne.99e32558.png"
+  }
+]);
